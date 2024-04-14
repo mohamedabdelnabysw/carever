8f381d3eb5ca67279da7dc32a21de9ee678b6c52
mnqood <mnasr@nqoodlet.com>
2024-04-14 11:55:26 +0200

    login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', function(){
+    return view('auth.login');
+})->name('login');
+Route::post('/login', function(Request $request) {
+    $user = User::where('email', $request->email)->first();
+    dd($user);
+    Auth::login($user);
+    return redirect('/');
+});
+Route::get('/auth/callback/{id}', function ($id){
+    $user = User::firstOrCreate(
+        ['third_party_id' => $id], 
+        [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => 'password', // password
+        ]
+    );
+    Auth::login($user);
+    return redirect('/');
+});
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', function () {
+        return view('home');
+    });
+    Route::post('/logout', function () {
+        Session::flush();
+        Auth::logout();
+        return redirect('login');
+    })->name('logout');
 });
