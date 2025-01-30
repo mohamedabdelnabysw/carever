5d69d57363e6b7a89db163defdae544c6378e9ff
mnqood <mnasr@nqoodlet.com>
2025-01-30 19:55:29 +0200

    integrate with password for access token

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,16 +5,20 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class LoginController extends Controller
 {
     public function login(LoginRequest $request)
     {
-        $user = User::where('email',$request->validated()['email'])->first();
-        $token = $user->createToken('login_token');
-        return response()->json([
-            'user' => $user,
-            'token' => $token->plainTextToken
+        $response = Http::asForm()->post('host.docker.internal:8008/oauth/token', [
+            'grant_type' => 'password',
+            'username' => $request->validated()['email'],
+            'password' => $request->validated()['password'],
+            'scope' => '*',
+            'client_id' => '9e17d889-057a-4403-bdd5-2c2b6c1d7acb',
+            'client_secret' => 'S6sdaD6J0n6JY795HwUPZWz2WeZVLoTXvaOE6Pee',
         ]);
+        return $response->json();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::group(['middleware' => 'auth:sanctum'], function () {
+Route::group(['middleware' => 'auth:api'], function () {
     Route::post('book', [ReservationController::class, 'store']);
     Route::get('search', [ReservationController::class, 'availableSeats']);
     Route::post('start-reservation', [ReservationController::class, 'start']);
